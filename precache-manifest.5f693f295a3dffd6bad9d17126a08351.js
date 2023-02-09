3edef96d9577eb6ce86a51dc5f019abc392b5f10
Cristian <cmachuca32@gmail.com>
2023-02-09 11:32:42 -0800

    Updates

---
+++ precache-manifest.5f693f295a3dffd6bad9d17126a08351.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "74b9a75aafad569a2cea",
+    "url": "/cristian1534/github.io/jenkins-useReducer/static/js/main.74b9a75a.chunk.js"
+  },
+  {
+    "revision": "8f3f91fd6de07c61115f",
+    "url": "/cristian1534/github.io/jenkins-useReducer/static/js/1.8f3f91fd.chunk.js"
+  },
+  {
+    "revision": "9804c2dc6769f81178e7",
+    "url": "/cristian1534/github.io/jenkins-useReducer/static/js/runtime~main.9804c2dc.js"
+  },
+  {
+    "revision": "f6ed8c8b154f7804067da8731b9203fe",
+    "url": "/cristian1534/github.io/jenkins-useReducer/index.html"
+  }
+];
